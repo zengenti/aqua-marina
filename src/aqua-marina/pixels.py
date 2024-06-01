a2f27dac206a7aeb7b1558c5dfe91f142e3dce2d
Joe J Collins <joejcollins@users.noreply.github.com>
2024-06-01 21:13:13 +0000

    Remove comments

--- src/aqua-marina/pixels.py
+++ src/aqua-marina/pixels.py
@@ -15,24 +15,9 @@ import skimage as ski
 eight = iio.imread(uri='../data/raw/training/eight.tif')
 print(type(eight))
 
-# %% [markdown]
-# Load some more libraries.
-
-# %%
-
-
-# %% [markdown]
-# Renaming the imports seems to be a thing.  At least for `numpy` being renamed to `np`.
-
-# %%
-%matplotlib widget
 fig, ax = pyplot.subplots()
 ax.imshow(eight)
 
-# %% [markdown]
-# Let's look at the pixels in the image.
-
-# %%
 print(eight.shape)
 print(eight)
 
